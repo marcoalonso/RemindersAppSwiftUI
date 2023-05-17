8557f44fce7e1b4b6fcd52efa526bc3144a9ee61
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-05-17 14:06:36 -0600

    15 Implementig Reminder CellView

--- RemindersApp/Preview Content/PreviewData.swift
+++ RemindersApp/Preview Content/PreviewData.swift
@@ -6,10 +6,15 @@
 //
 
 import Foundation
-
 import CoreData
 
 class PreviewData {
+    
+    static var reminder : Reminder {
+        let viewContext = CoreDataProvider.shared.persistentContainer.viewContext
+        let request = Reminder.fetchRequest()
+        return(try? viewContext.fetch(request).first) ?? Reminder(context: viewContext)
+    }
     
     static var myList: MyList {
         let viewContext = CoreDataProvider.shared.persistentContainer.viewContext
