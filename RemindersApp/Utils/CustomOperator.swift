752324bee25d878fa8aa438a6bf818f1da6342b2
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-05-18 13:21:06 -0600

    16 Implementing Reminder Detail View

---
+++ RemindersApp/Utils/CustomOperator.swift
@@ -0,0 +1,16 @@
+//
+//  CustomOperator.swift
+//  RemindersApp
+//
+//  Created by Marco Alonso Rodriguez on 18/05/23.
+//
+
+import Foundation
+import SwiftUI
+
+public func ??<T>(lhs: Binding<Optional<T>>, rhs: T) -> Binding<T> {
+    Binding(
+        get: { lhs.wrappedValue ?? rhs },
+        set: { lhs.wrappedValue = $0 }
+    )
+}
---
+++ RemindersApp/Views/ReminderDetailView.swift
@@ -0,0 +1,78 @@
+//
+//  ReminderDetailView.swift
+//  RemindersApp
+//
+//  Created by Marco Alonso Rodriguez on 18/05/23.
+//
+
+import SwiftUI
+
+struct ReminderDetailView: View {
+    
+    @Environment(\.dismiss) private var dismiss
+    
+    @Binding var reminder: Reminder
+    @State var editConfig: ReminderEditConfig = ReminderEditConfig()
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    TextField("Title", text: $editConfig.title)
+                    TextField("Notes", text: $editConfig.notes ?? "")
+                }
+                
+                Section {
+                    Toggle(isOn: $editConfig.hasDate) {
+                        Image(systemName: "calendar")
+                            .foregroundColor(.red)
+                    }
+                    
+                    if editConfig.hasDate {
+                        DatePicker("Select Date", selection: $editConfig.reminderDate ?? Date(), displayedComponents: .date)
+                        
+                    }
+                    
+                    Toggle(isOn: $editConfig.hasTime) {
+                        Image(systemName: "clock")
+                            .foregroundColor(.blue)
+                    }
+                    
+                    if editConfig.hasTime {
+                        DatePicker("Select Date", selection: $editConfig.reminderTime ?? Date(), displayedComponents: .hourAndMinute)
+                        
+                    }
+                    
+                }
+            }.listStyle(.insetGrouped)
+        }
+        .toolbar {
+            ToolbarItem(placement: .principal) {
+                Text("Details")
+            }
+            
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button("Done") {
+                    
+                }
+            }
+            
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button("Cancel") {
+                    dismiss()
+                }
+            }
+        }
+        .onAppear {
+            editConfig = ReminderEditConfig(reminder: reminder)
+        }
+        
+    }
+}
+
+struct ReminderDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReminderDetailView(reminder: .constant(PreviewData.reminder))
+        
+    }
+}
